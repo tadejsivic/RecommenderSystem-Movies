b0eca27c7dc4d8e5c5573da724eeb7f1b9fd4647
tadejsivic <tadej.sivic@gmail.com>
2022-12-28 12:39:28 +0100

    ItemBasedPredictor now works. Does not predict yet.

--- ItemBasedPredictor.py
+++ ItemBasedPredictor.py
@@ -4,6 +4,7 @@ from UserItemData import UserItemData
 from MovieData import MovieData
 
 import numpy as np
+from numpy import linalg
 import pandas as pd
 import math
 
@@ -21,78 +22,56 @@ class ItemBasedPredictor(Predictor):
         self.frame["rec_rating"] = 0
 
         self.similar_movies = {}
-        for p1 in np.unique(np.array(self.frame["movieID"])):
-            for p2 in np.unique(np.array(self.frame["movieID"])):
+        unique_movies = self.frame["movieID"].unique()
+        user_averages_data = self.frame.groupby(["userID"])["rating"].mean()
+        user_averages = dict(zip( user_averages_data.index.tolist(), user_averages_data.values.tolist() ))
+
+        for p1 in unique_movies:
+            for p2 in unique_movies:
                 if p1 == p2:
                     continue
+                if (p1,p2) in self.similar_movies: 
+                    self.similar_movies[(p2,p1)] = self.similar_movies[(p1,p2)]
+                    continue
+                elif (p2,p1) in self.similar_movies:
+                    self.similar_movies[(p1,p2)] = self.similar_movies[(p2,p1)]
+                    continue
+                
+                # Similarity
+                first_movie = self.frame[self.frame["movieID"] == p1]
+                second_movie = self.frame[self.frame["movieID"] == p2]
+                users_both_movies = np.intersect1d( first_movie["userID"].unique(), second_movie["userID"].unique() )
+                if len(users_both_movies) < self.min_values:
+                    self.similar_movies[(p1,p2)] = 0
+                    continue
 
-                if (p2, p1) in self.similar_movies.keys() :
-                    self.similar_movies[(p1, p2)] = self.similar_movies[(p2, p1)]
-                else: 
-                    self.similar_movies[(p1, p2)] = self.similarity(p1, p2)
-
+                first_movie = first_movie.loc[first_movie["userID"].isin(users_both_movies)]
+                second_movie = second_movie.loc[second_movie["userID"].isin(users_both_movies)]
+                
+                user_averages_ratings = [user_averages[x] for x in users_both_movies]
+                first_movie_ratings = np.array(first_movie["rating"].tolist()) - user_averages_ratings
+                second_movie_ratings = np.array(second_movie["rating"].tolist()) - user_averages_ratings
+                
+                # We calculate the similarity using the adjusted cosine similarity formula                
+                sim = np.dot(first_movie_ratings, second_movie_ratings) / (np.linalg.norm(first_movie_ratings) * np.linalg.norm(second_movie_ratings))
+                if sim < self.threshold: # Insignificant similarity
+                    self.similar_movies[(p1,p2)] = 0
+                    continue
+                self.similar_movies[(p1,p2)] = sim
+                
 
     def predict(self, user_id, num):
         pass
 
 
     def similarity(self, p1, p2):
-        if p1 == p2:
-            return 0
-        # p1 and p2 are essentially movie_id1 and movie_id2
-        # sim is the similarity score calculated using adjusted cosine similarity
-        mask = (self.frame["movieID"] == p1) | (self.frame["movieID"] == p2)
-        temporary_grand = self.frame[mask]    
-        
-        # First we get only proper ratings, so users who rated both movies
-        temp_frame = pd.DataFrame()
-        for user_id in np.unique(np.array(temporary_grand["userID"])):
-            users_rated_movies = temporary_grand.loc[(temporary_grand["userID"] == user_id) & ((temporary_grand["movieID"] == p1) | (temporary_grand["movieID"] == p2)), ]
-            if len(users_rated_movies) == 2:
-                temp_frame = pd.concat([temp_frame, users_rated_movies])
-        
-        if len(temp_frame)/2 < self.min_values: # Not enough user ratings
-            return 0
+        return self.similar_movies[(p1,p2)]
 
-        # Then, we calculate the similarity using the adjusted cosine similarity formula
-        numerator = 0
-        denom1 = 0
-        denom2 = 0
-        
-        if len(temp_frame) == 0:
-            return 0
-        
-        for user_id in np.unique(np.array(temp_frame["userID"])):
-            users_ratings = np.array(temp_frame.loc[temp_frame["userID"] == user_id, "rating"])
-            users_average =  np.average(self.frame.loc[self.frame["userID"] == user_id, "rating"])
-            
-            numerator += (users_ratings[0] - users_average) * (users_ratings[1] - users_average)
-            denom1 += math.pow(users_ratings[0] - users_average, 2)
-            denom2 += math.pow(users_ratings[1] - users_average, 2)
-        denom1 = math.sqrt(denom1)
-        denom2 = math.sqrt(denom2)
-
-        sim = numerator / (denom1 * denom2)
-        
-        if sim < self.threshold: # Insignificant similarity
-            return 0
-        return sim
-    
 
 ibp = ItemBasedPredictor()
 ibp.fit(UserItemData("data/user_ratedmovies.dat", min_ratings=1000))
 
-if (1580,780) in ibp.similar_movies.keys():
-    print("1580, 780 is")
-else:
-    print("1580, 780 is nowhere to be found")
-if (780,1580) in ibp.similar_movies.keys():
-    print("780, 1580 is")
-else:
-    print("780, 1580 is nowhere to be found")
-
-#print(ibp.similar_movies[(1,32)])
-print(ibp.similar_movies[(1580, 780)])
+print(ibp.similarity(1580, 780))
 
 
 
